59af3c76ad8616878ffb62d1f8a41168ba95a62c
monysia02 <moniamonika.b02@gmail.com>
2025-03-26 12:07:59 +0100

    final code

--- upload_s3_python.py
+++ upload_s3_python.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-"
+# -*- coding: utf8 -*-
 
 import boto3
 from botocore.client import ClientError
@@ -15,11 +15,11 @@ ec2InsDatafile = 'ec2InsDatafile'
 ec2_params = {
     'Instance ID': 'instance-id',
     'Reservation ID': 'reservation-id',
-    'Public IP': 'public_ipv4',
-    'Public Hostname': 'public_hostname',
+    'Public IP': 'public-ipv4',
+    'Public Hostname': 'public-hostname',
     'Private IP':'local-ipv4',
     'Security Groups':'security-groups',
-    'AMI ID': 'ami_id'
+    'AMI ID': 'ami-id'
 }
 
 try:
@@ -34,9 +34,11 @@ for param, value in ec2_params.items():
         print("Error while making request")
     if isinstance(response.text,list):
         print(response.text +': is list')
-        data = ' '.joint(response.text)
+        data = ' '.join(response.text)
     else:
         data = param +":"+response.text
+    if param == 'Instance ID':
+        instance_id = response.text.strip()
     try:
           fh.write(data+'\r\n')
     except:
@@ -49,7 +51,7 @@ os_vers = "grep '^VERSION=' /etc/os-release |cut -d'=' -f2"
 os_name = "grep '^NAME' /etc/os-release |cut -d'=' -f2"
 os_name_val ='OS NAME: '+ os.popen(os_name).read().rstrip()
 os_vers_val ='OS VERSION: '+ os.popen(os_vers).read().rstrip()
-os_usrs = "grep -E 'bash|sh' /etc/passwd |awk -F : '{print $1}|xargs echo  "
+os_usrs = "grep -E 'bash|sh' /etc/passwd |awk -F : '{print $1}' |xargs echo  "
 os_usrs_val = 'Login able users: '+ os.popen(os_usrs).read().rstrip()
 try:
     fh.write(os_name_val+'\r\n')
@@ -65,12 +67,12 @@ s3_bucket_name = 'new-bucket-e05ab0e0'
 s3_conn = boto3.client('s3')
 
 try:
-    s3.meta.client.head_bucket(Bucket=s3_bucket_name)
+    #s3.meta.client.head_bucket(Bucket=s3_bucket_name)
 
-    with (ec2InsDatafile, 'r') as fh:
+    with open(ec2InsDatafile, 'r') as fh:
         s3_conn.put_object(
             Bucket=s3_bucket_name,
-            Key='system_info' + requests.get(meta_data +'/' + instance_id) + '.txt',
+            Key='system_info' + instance_id + '.txt',
             Body=fh.read()
         )
     print("File has been uploaded into " + s3_bucket_name + " S3 bucket with instance_id key.")
